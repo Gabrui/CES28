138fd1bc63c9ef4759a9545fb2702b0db87b0ea7
DylanNS <dylan-ns@hotmail.com>
2017-08-02 08:37:23 -0300

    new code

--- Lab1/CES_28_LAB_1/codigo/comp19_Lab1_CES28_Dylan_Nakandakari_Sugimoto-Gabriel_Adriano_de_Melo/src/teste/TesteMoney.java
+++ Lab1/CES_28_LAB_1/codigo/comp19_Lab1_CES28_Dylan_Nakandakari_Sugimoto-Gabriel_Adriano_de_Melo/src/teste/TesteMoney.java
@@ -10,6 +10,8 @@ package teste;
 import main.Money;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Before;
+
 /**
  * Class TesteMoney: Possui metodos que testam a classe Money
  * 
@@ -21,6 +23,8 @@ public class TesteMoney {
 	private Money EUR19;
 	private Money GBP7;
 	private int maxint;
+	
+	@Before
 	//Definindo alguns objetos Money para usar nos testes
 	public void setUp() {
 		BRL7 = new Money(7,"BRL");
@@ -28,6 +32,52 @@ public class TesteMoney {
 		EUR19 = new Money(19,"EUR");
 		GBP7 = new Money(7,"GBP");
 		maxint = 2147483647;
+	}
+	
+	@Test
+	public void VerificarMetodoEqualDaClasseMoney_QuandoCompararIguaisDevemSerIguais() {
+		//Criando objetos para usar no Teste
+		Money GBP07 = new Money(7,"GBP");
+		Money GBP0 = new Money(0,"GBP");
+		Money GBPmax = new Money(maxint,"GBP");
+		Money EUR019 = new Money(19,"EUR");
+		Money EUR7 = new Money(7,"EUR");
+		//Fim da Criacao de Objetos
+		
+		//Verificando se sao iguais pelo metodo Equals
+		assertEquals(GBP7,GBP07);
+		assertEquals(GBP0,GBP0);
+		assertEquals(GBP7,GBP7);
+		assertEquals(GBPmax,GBPmax);
+		assertEquals(EUR019,EUR019);
+		assertEquals(EUR19,EUR019);
+		//repetindo
+		assertTrue(GBP7.equals(GBP07));
+		assertTrue(GBP7.equals(GBP7));
+		assertTrue(GBP0.equals(GBP0));
+		assertTrue(EUR019.equals(EUR019));
+		assertTrue(EUR019.equals(EUR19));
+		assertTrue(GBPmax.equals(GBPmax));
+		
+	}
+	
+	@Test
+	public void VerificarMetodoEqualDaClasseMoney_QuandoComparaDiferenteDevemSerDiferentes() {
+		//Criando objetos para usar no Teste
+				Money GBP07 = new Money(7,"GBP");
+				Money GBP0 = new Money(0,"GBP");
+				Money GBPmax = new Money(maxint,"GBP");
+				Money EUR019 = new Money(19,"EUR");
+				Money EUR7 = new Money(7,"EUR");
+				//Fim da Criacao de Objetos
+				
+		//conferindo quando sao diferentes
+				assertTrue(!(GBP7.equals(GBP0)));
+				assertTrue(!(GBP7.equals(EUR7)));
+				assertTrue(!(EUR019.equals(GBP7)));
+				assertTrue(!(GBP7.equals(GBPmax)));
+				assertTrue(!(GBPmax.equals(EUR19)));
+		
 	}
 	
 	@Test
