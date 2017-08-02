b768bb635922578a2261ba108783635efb58ba32
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2017-08-02 20:31:30 -0300

    Basico pronto

--- Lab1/CES_28_LAB_1/codigo/comp19_Lab1_CES28_Dylan_Nakandakari_Sugimoto-Gabriel_Adriano_de_Melo/src/main/MoneyBag.java
+++ Lab1/CES_28_LAB_1/codigo/comp19_Lab1_CES28_Dylan_Nakandakari_Sugimoto-Gabriel_Adriano_de_Melo/src/main/MoneyBag.java
@@ -3,41 +3,66 @@
  */
 package main;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+
 
 /**
  * @author Gabriel
  *
  */
 public class MoneyBag implements Pocket{
-	List<Money> myList = new ArrayList<Money>();
+	private List<Money> lista;
 	/**
 	 * 
 	 */
 	public MoneyBag() {
-		// TODO Auto-generated constructor stub
+		this.lista = new LinkedList<Money>();
 	}
-
+	
+	
 	@Override
 	public Pocket add(Money money) {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.hasMoney(money))
+		this.lista.add(money);
+		return this;
 	}
 
+	
 	@Override
 	public int getAmount() {
-		// TODO Auto-generated method stub
-		return 0;
+		int total = 0;
+		for (Money dim : this.lista) {
+			total += dim.getAmount() * dim.getCurrency().getTaxaDeCambioParaReais();
+		}
+		return total;
 	}
 	
-	public boolean hasMoney() {
-		return true;
+	
+	public boolean hasMoney(Money money) {
+		return this.lista.contains(money);
 	}
+	
+	
+	public boolean hasCurrency(Currency curr) {
+		for (Money dim : this.lista) {
+			if (dim.getCurrency().equals(curr))
+				return true;
+		}
+		return false;
+	}
+	
 	
 	@Override
-	public boolean equals(Object m) {
-		return false;
+	public boolean equals(Object moneyBag) {
+		if (moneyBag instanceof MoneyBag) {
+			MoneyBag m = (MoneyBag) moneyBag;
+			for (Money dim : this.lista) {
+				if (!m.hasMoney(dim))
+					return false;
+			}
+		}
+		return true;
 	}
 
 }
