f58d7800fec4f98d59f382aaecc76a5d652dd520
DylanNS <dylan-ns@hotmail.com>
2017-08-02 19:48:41 -0300

    teste do equal da classe MoneyBag

--- Lab1/CES_28_LAB_1/codigo/comp19_Lab1_CES28_Dylan_Nakandakari_Sugimoto-Gabriel_Adriano_de_Melo/src/teste/TesteMoneyBag.java
+++ Lab1/CES_28_LAB_1/codigo/comp19_Lab1_CES28_Dylan_Nakandakari_Sugimoto-Gabriel_Adriano_de_Melo/src/teste/TesteMoneyBag.java
@@ -34,7 +34,7 @@ public class TesteMoneyBag {
 	
 	/**
 	 * Este metodo realiza algumas preparacoes para a realizacao do teste
-	 * 
+	 * Passo 8
 	 */
 	@Before
 	public void setUp() {
@@ -48,4 +48,49 @@ public class TesteMoneyBag {
 		maxint = 2147483647;
 	}
 	
+	/**
+	 * Esse metodo compara MoneyBag iguais
+	 * Passo 8
+	 */
+	@Test
+	public void VerificarMetodoEqualDaClasseMoney_QuandoCompararMoneyIguaisDevemSerIguais() {
+		//Criando objetos para usar no Teste
+		Money USD7 = new Money(7,USD);
+		Money USD0 = new Money(0,USD);
+		Money USDmax = new Money(maxint,USD);
+		Money EUR0 = new Money(0,EUR);
+		Money EUR7 = new Money(7,EUR);
+		Money EURmax = new Money(maxint, EUR);
+		MoneyBag equalbagtest = new MoneyBag();
+		MoneyBag bagtest2 = new MoneyBag();
+		MoneyBag unibag2 = new MoneyBag(); 
+		//Fim da Criacao de Objetos
+		
+		bagtest.add(USD7);
+		bagtest.add(USD0);
+		bagtest.add(EUR0);
+		bagtest.add(EUR7);
+		bagtest.add(EUR7);
+		unibag.add(EURmax);
+		
+		equalbagtest.add(USD7);
+		equalbagtest.add(USD0);
+		equalbagtest.add(EUR0);
+		equalbagtest.add(EUR7);
+		equalbagtest.add(EUR7);
+		unibag2.add(EURmax);
+		bagtest2 = bagtest;
+		
+		//Verificando se sao iguais pelo metodo Equals
+		assertEquals(equalbagtest,bagtest);
+		assertEquals(bagtest,equalbagtest);
+		assertEquals(bagtest,bagtest);
+		assertEquals(bagtest2,bagtest);
+		assertEquals(bagtest,bagtest2);
+		assertEquals(unibag,unibag);
+		assertEquals(emptybag,emptybag);
+		assertEquals(unibag2,unibag);
+		assertEquals(unibag,unibag2);
+		
+	}
 }
