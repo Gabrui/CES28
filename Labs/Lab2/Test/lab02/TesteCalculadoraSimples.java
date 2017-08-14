ede0e0c63c46b9e382c2ad3bc7b80a9c234bf498
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2017-08-14 17:30:20 -0300

    Testes e implementação simples

--- Labs/Lab2/Test/lab02/TesteCalculadoraSimples.java
+++ Labs/Lab2/Test/lab02/TesteCalculadoraSimples.java
@@ -15,5 +15,23 @@ public class TesteCalculadoraSimples {
 	public void QuandoStringVaziaRetornaZero() {
 		assertEquals(0, CalculadoraString.add(""));
 	}
+	
+	@Test
+	public void QuandoTemUmNumeroRetornaEsseNumero() {
+		assertEquals(4, CalculadoraString.add("4"));
+		assertEquals(442, CalculadoraString.add("442"));
+	}
+	
+	@Test
+	public void SomaNumerosSeparadosPorVirgula() {
+		assertEquals(3, CalculadoraString.add("1,2"));
+		assertEquals(10, CalculadoraString.add("1,2,3,4"));
+	}
+	
+	@Test
+	public void SomaNumerosSeparadosPorMarcadoresPadroes() {
+		assertEquals(3, CalculadoraString.add("    1    \n, 2  "));
+		assertEquals(10, CalculadoraString.add("1   ,  2\n3 4   "));
+	}
 
 }
--- Labs/Lab2/src/lab02/CalculadoraString.java
+++ Labs/Lab2/src/lab02/CalculadoraString.java
@@ -3,7 +3,20 @@ package lab02;
 public class CalculadoraString {
 
 	public static int add(String operandos) {
-		return 0;
+		if (operandos.isEmpty())
+			return 0;
+		int total = 0;
+		String[] numeros = operandos.split("[, \n]");
+		for(String numero : numeros) {
+			total += CalculadoraString.getValor(numero);
+		}
+		return total;
+	}
+	
+	private static int getValor(String numero) {
+		if (numero.isEmpty())
+			return 0;
+		return Integer.valueOf(numero);
 	}
 
 }
