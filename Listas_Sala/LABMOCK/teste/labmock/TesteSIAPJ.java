15dc55434af757a6fcd2d844a9c4fb8dc78a44e7
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2017-08-22 09:58:32 -0300

    Refatoração do Código

--- Listas_Sala/LABMOCK/teste/labmock/TesteSIAPJ.java
+++ Listas_Sala/LABMOCK/teste/labmock/TesteSIAPJ.java
@@ -1,17 +1,27 @@
 package labmock;
 
-import static org.mockito.Mockito.mock;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class TesteSIAPJ {
+	
+	// Refatorei utilizando as anotações, antes tinha criado os objetos
+	// no próprio método.
+	@Mock private IServicoEmail emailMock;
+	@Mock private IServicoDB dbMock;
+	@Mock private IValidatorProcesso procValMock;
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
 
 	@Test
-	public void test() {
-		IServicoEmail emailMock = mock(IServicoEmail.class);
-		IServicoDB dbMock = mock(IServicoDB.class);
-		IValidatorProcesso procValMock  = mock(IValidatorProcesso.class);
+	public void testaConstrutor() {
 		SIAPJ objTeste = new SIAPJ(emailMock, dbMock, procValMock);
 		assertNotNull(objTeste);
 	}
