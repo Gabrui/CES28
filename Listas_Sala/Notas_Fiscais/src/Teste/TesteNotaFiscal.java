ec8f0272c119ea0df68be3e004adcd40af76776a
DylanNS <dylan-ns@hotmail.com>
2017-08-28 15:56:51 -0300

    create class teste

--- Listas_Sala/Notas_Fiscais/src/Teste/TesteNotaFiscal.java
+++ Listas_Sala/Notas_Fiscais/src/Teste/TesteNotaFiscal.java
@@ -5,11 +5,21 @@
  * Dia: 28/08/2017
  */
 package Teste;
-
+import org.junit.Assert;
+import org.junit.Test;
 /**
  * 
  *
  */
 public class TesteNotaFiscal {
 
+	
+	
+	/**
+	 * Testa cadastro de cliente com CPF limpo
+	 */
+	@Test
+	public void QuandoPassaCPFLimpoVerificadorCadastroRetornaTrue() {
+		
+	}
 }
