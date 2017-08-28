8f926341b9c3f8150a5718076d74c65467eeddcf
DylanNS <dylan-ns@hotmail.com>
2017-08-28 16:57:35 -0300

    teste do construtor da classe NotaFiscal

--- Listas_Sala/Notas_Fiscais/src/Teste/TesteNotaFiscal.java
+++ Listas_Sala/Notas_Fiscais/src/Teste/TesteNotaFiscal.java
@@ -5,9 +5,16 @@
  * Dia: 28/08/2017
  */
 package Teste;
-import org.junit.Assert;
+
+import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Before;
 import org.mockito.Mockito;
+import org.mockito.Mock;
+import db.VerificadorCPF;
+import db.BDCliente;
+import notaFiscal.NotaFiscal;
+import notaFiscal.Cadastro;
 
 /**
  * 
@@ -17,12 +24,42 @@ public class TesteNotaFiscal {
 
 	
 	
+	@Mock private VerificadorCPF fiscalizador;
+	@Mock private BDCliente bdc;
+	
+	/**
+	 * Declaracao de variaveis para uso nos testes
+	 */
+	private Cadastro casd = new Cadastro(bdc,fiscalizador);
+	private String cpf;
+	private int quant;
+	private String item ;
+	@Before
+	public void setUp() {
+		 cpf = "10";
+		 quant= 10;
+		 item = "Notebook";
+	}
+	
 	/**
 	 * Testa cadastro de cliente com CPF limpo
 	 */
 	@Test
-	public void QuandoPassaCPFLimpoVerificadorCPFRetornaTrue() {
-		VerificadorCPF fiscalizador = new VerificadorCPF();
-		assertTrue(fiscalizador.);
+	public void QuandoPassaCPFLimpoNotaFiscalPodeSerInstaciada() {
+		
+		Mockito.when(fiscalizador.validaCPF(cpf)).thenReturn(true);
+		assertTrue(fiscalizador.validaCPF(cpf));
+		NotaFiscal testeNotaFiscal = new NotaFiscal(casd,cpf,item,quant);
 	}
+	
+	/**
+	 * Testa cadastro de cliente com CPF sujo
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void QuandoPassaCPFSujoNotaFiscalRetornaIllegalArgumentExceptionNaInstanciacao() {
+		Mockito.when(fiscalizador.validaCPF(cpf)).thenReturn(false);
+		assertFalse(fiscalizador.validaCPF(cpf));
+		NotaFiscal testeNotaFiscal = new NotaFiscal(casd,cpf,item,quant);
+	}
+	
 }
