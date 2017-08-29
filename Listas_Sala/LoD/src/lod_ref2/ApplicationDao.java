95dfbd27779a2b96bb64eb3e3554ffda6829e8a2
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2017-08-29 09:48:35 -0300

    Refatoração 3

---
+++ Listas_Sala/LoD/src/lod_ref2/ApplicationDao.java
@@ -0,0 +1,10 @@
+package lod_ref2;
+
+public class ApplicationDao {
+
+	public ApplicationResult persist(Applicant applicant) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ Listas_Sala/LoD/src/lod_ref2/ApplicationValidation.java
@@ -0,0 +1,12 @@
+package lod_ref2;
+
+public class ApplicationValidation {
+
+	public ApplicationResult validate(Applicant applicant) {
+		return null;
+	}
+	
+	
+
+
+}
