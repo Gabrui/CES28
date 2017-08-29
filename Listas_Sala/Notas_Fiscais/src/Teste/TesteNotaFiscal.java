297c5d743308de7233fd7089b7d7fb4a357c063b
DylanNS <dylan-ns@hotmail.com>
2017-08-29 08:08:16 -0300

    mockado classe cadastro

--- Listas_Sala/Notas_Fiscais/src/Teste/TesteNotaFiscal.java
+++ Listas_Sala/Notas_Fiscais/src/Teste/TesteNotaFiscal.java
@@ -10,11 +10,14 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Before;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.Mock;
 import db.VerificadorCPF;
 import db.BDCliente;
+import db.Cliente;
 import notaFiscal.NotaFiscal;
 import notaFiscal.Cadastro;
+import notaFiscal.ItemVenda;
 
 /**
  * 
@@ -26,11 +29,14 @@ public class TesteNotaFiscal {
 	
 	@Mock private VerificadorCPF fiscalizador;
 	@Mock private BDCliente bdc;
+	@Mock private ItemVenda produtoVendido;
+	@Mock private Cadastro casd;
+	@Mock private Cliente cliente;
 	
 	/**
 	 * Declaracao de variaveis para uso nos testes
 	 */
-	private Cadastro casd = new Cadastro(bdc,fiscalizador);
+	
 	private String cpf;
 	private int quant;
 	private String item;
@@ -40,6 +46,7 @@ public class TesteNotaFiscal {
 	
 	@Before
 	public void setUp() {
+		MockitoAnnotations.initMocks(this);
 		 cpf = "10";
 		 quant= 10;
 		 item = "item";
@@ -53,9 +60,12 @@ public class TesteNotaFiscal {
 	 */
 	@Test
 	public void QuandoPassaCPFLimpoNotaFiscalPodeSerInstaciada() {
-		
+		Mockito.when(casd.getCliente(cpf)).thenReturn(cliente);
 		Mockito.when(fiscalizador.validaCPF(cpf)).thenReturn(true);
+		
 		assertTrue(fiscalizador.validaCPF(cpf));
+		Mockito.verify(fiscalizador, Mockito.times(1)).validaCPF(cpf);
+		
 		NotaFiscal testeNotaFiscal = new NotaFiscal(casd,cpf,item,quant);
 	}
 	
@@ -65,7 +75,11 @@ public class TesteNotaFiscal {
 	@Test(expected = IllegalArgumentException.class)
 	public void QuandoPassaCPFSujoNotaFiscalRetornaIllegalArgumentExceptionNaInstanciacao() {
 		Mockito.when(fiscalizador.validaCPF(cpf)).thenReturn(false);
+		Mockito.when(casd.getCliente(cpf)).thenReturn(cliente);
+		
 		assertFalse(fiscalizador.validaCPF(cpf));
+		Mockito.verify(fiscalizador, Mockito.times(1)).validaCPF(cpf);
+		
 		NotaFiscal testeNotaFiscal = new NotaFiscal(casd,cpf,item,quant);
 	}
 	
@@ -95,7 +109,7 @@ public class TesteNotaFiscal {
 		testeNotaFiscal.deletaItem(item);
 		
 		assertTrue(testeNotaFiscal.getItem(livro)== quant);
-		asserTrue(testeNotaFiscal.getItem(item) == 0);
+		assertTrue(testeNotaFiscal.getItem(item) == 0);
 		
 		testeNotaFiscal.adicionaItem(notebook, 27);
 		testeNotaFiscal.adicionaItem(caneta, 7);
@@ -103,7 +117,7 @@ public class TesteNotaFiscal {
 		
 		assertTrue(testeNotaFiscal.getItem(notebook) == 27);
 		assertTrue(testeNotaFiscal.getItem(caneta) == 7);
-		asserTrue(testeNotaFiscal.getItem(item) == quant);
+		assertTrue(testeNotaFiscal.getItem(item) == quant);
 		
 		testeNotaFiscal.deletaItem(notebook);
 		testeNotaFiscal.deletaItem(livro);
@@ -111,10 +125,10 @@ public class TesteNotaFiscal {
 		testeNotaFiscal.deletaItem(item);
 		
 		assertTrue(testeNotaFiscal.getItem(livro)== 0);
-		asserTrue(testeNotaFiscal.getItem(item) == 0);
+		assertTrue(testeNotaFiscal.getItem(item) == 0);
 		assertTrue(testeNotaFiscal.getItem(livro) == 0);
 		assertTrue(testeNotaFiscal.getItem(caneta) == 0);
-		asserTrue(testeNotaFiscal.getItem(notebook) == 0);
+		assertTrue(testeNotaFiscal.getItem(notebook) == 0);
 	}
 	
 	/**
