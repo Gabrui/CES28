ba8768f110073fa49d4615933313d76b4e74ecb4
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2017-09-05 10:15:54 -0300

    Fim do item 2

--- Listas_Sala/SRP_SOLID/src/item2_solucao/Checking.java
+++ Listas_Sala/SRP_SOLID/src/item2_solucao/Checking.java
@@ -2,6 +2,7 @@ package item2_solucao;
 
 public class Checking extends BankAccount {
 	public void Transfer(double amount, BankAccount toAccount) {
-		
+		this.Withdraw(amount);
+		toAccount.Deposit(amount);
 	};
 }
--- Listas_Sala/SRP_SOLID/src/item2_solucao/Savings.java
+++ Listas_Sala/SRP_SOLID/src/item2_solucao/Savings.java
@@ -2,6 +2,6 @@ package item2_solucao;
 
 public class Savings extends BankAccount {
 	public void AddInterest(double amount) {
-		
+		this.Deposit(amount);
 	};
 }
