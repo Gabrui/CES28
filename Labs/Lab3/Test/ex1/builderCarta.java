8a85682499d7ac006a55300269e2b9eefdb23b85
DylanNS <dylan-ns@hotmail.com>
2017-09-29 10:42:33 -0300

    builderCarta
    Esta classe é o buider da classe Carta.
    Ela serva para o usuario usar para construir a classe CartaX, em que X é
    o modelo da carta que deve ser passado como parametro antes de construir
    a carta.

---
+++ Labs/Lab3/Test/ex1/builderCarta.java
@@ -0,0 +1,21 @@
+/**
+ * @author Dylan N. Sugimoto e Gabriel Adriano de Melo
+ * Data da criação: 29/09/2017
+ */
+package ex1;
+
+/**
+ * Esta classe é o builder da classe Carta.
+ *
+ */
+public class builderCarta {
+	
+	private String cabecario;
+	private String corpo;
+	private String conclusao;
+	private String assinatura;
+	
+	public builderCarta() {
+		
+	}
+}
