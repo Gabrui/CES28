20a983c0944f8afb8c20e9e0cabc7ea2c6d9b7f1
DylanNS <dylan-ns@hotmail.com>
2017-09-29 10:55:25 -0300

    builderCarta
    forma primitiva. Somente assinatura das funções.

--- Labs/Lab3/Test/ex1/builderCarta.java
+++ Labs/Lab3/Test/ex1/builderCarta.java
@@ -10,12 +10,32 @@ package ex1;
  */
 public class builderCarta {
 	
-	private String cabecario;
-	private String corpo;
-	private String conclusao;
-	private String assinatura;
+	//Declarando variaveis privadas
+	private String _cabecario;
+	private String _corpo;
+	private String _conclusao;
+	private String _assinatura;
+	private String _modelo;
+	//Fim da declaração de variaveis privadas
 	
-	public builderCarta() {
+	//construtor da classe
+	public builderCarta(String modelo) {
+		this._modelo = modelo;
+	}
+	public void buildCabecario(String cabecario) {
+		this._cabecario = cabecario;
+	}
+	public void buildCorpo(String corpo) {
+		this._corpo = corpo;
+	}
+	public void buildConclusao(String conclusao) {
+		this._conclusao = conclusao;
+	}
+	public void buildAssinatura(String assinatura) {
+		this._assinatura = assinatura;
+	}
+	public Carta buildCarta() {
 		
+		return Carta(_cabecario,_corpo,_conclusao,_assinatura);
 	}
 }
