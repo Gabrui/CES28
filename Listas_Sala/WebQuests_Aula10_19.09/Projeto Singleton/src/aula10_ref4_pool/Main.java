e9102dead5d5b1ed53faddfeabacbcc7fbce2044
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2017-10-03 10:45:52 -0300

    Teste com comentários

--- Listas_Sala/WebQuests_Aula10_19.09/Projeto Singleton/src/aula10_ref4_pool/Main.java
+++ Listas_Sala/WebQuests_Aula10_19.09/Projeto Singleton/src/aula10_ref4_pool/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	// ---------- Operations --------------------
 	public static void        main(String[] args) throws Exception {
 		System.out.println(ClassLoader.getSystemClassLoader().loadClass("aula10_ref4_pool.Application").getClassLoader().getClass());
-		//Thread.sleep(8000);
+		
 		ApplicationTest t1 = new ApplicationTest();
 		ApplicationTest t2 = new ApplicationTest();
 		ApplicationTest t3 = new ApplicationTest();
@@ -15,9 +15,12 @@ public class Main {
 		Thread r3 = new Thread(t3);
 		System.out.println("Programa inicializado!");
 		r1.start();
+		System.out.println("Thread 2 vai falhar");
 		r2.start();
 
-		//r3.start();
+		Thread.sleep(20000);
+		System.out.println("Thread 3 começou, pegando só objeto reutilizados");
+		r3.start();
 	}  // method: main
 
 } // class: Main
