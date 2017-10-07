6a0dd61f7067bcf0bd42a018b7d0d738f69e3a45
DylanNS <dylan-ns@hotmail.com>
2017-10-07 14:31:06 -0300

    Class Telefone

---
+++ Labs/Lab3/src/ex2/Telefone.java
@@ -0,0 +1,27 @@
+/**
+ * LAB-3 / CES - 28
+ * @author Dylan N. Sugimoto e Gabriel Adriano de Melo
+ * Data da criacao: 07/10/2017
+ */
+package ex2;
+
+/**
+ * @author Dylan N. Sugimoto e Gabriel Adriano de Melo
+ *
+ */
+public class Telefone {
+
+	private String _DDI; //
+	private String _cc; //Country Code
+	private String _lc; //Local Code
+	private String _tel; // Telefone
+	
+	public Telefone(int DDI, int cc, int lc, int tel) {
+		
+		_DDI = String.valueOf(DDI);
+		_cc = String.valueOf(cc);
+		_lc = String.valueOf(lc);
+		String telefone = String.valueOf(tel);
+		_tel = telefone.substring(0 ,telefone.length() - 5) + "-" +  telefone.substring(telefone.length() - 5);
+	}
+}
