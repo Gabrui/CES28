7e8293b0926b5169ff6b3064597bccee46a6e742
DylanNS <dylan-ns@hotmail.com>
2017-10-07 13:30:56 -0300

    Class Idioma

---
+++ Labs/Lab3/src/ex2/Idioma.java
@@ -0,0 +1,28 @@
+/**
+ * LAB-3 / CES-28
+ * @author Dylan N. Sugimoto e Gabriel Adriano de Melo
+ * Data da criacao: 07/10/2017
+ */
+package ex2;
+
+/**
+ * @author Dylan N. Sugimoto e Gabriel Adriano de Melo
+ * Data da criacao: 07/10/2017
+ * Representacao  da traducao de alguns parametros da Carta.
+ */
+public class Idioma {
+	
+	private String _despedida;
+	private String _vocativo;
+	private String _PronomeTratamento;
+	
+	public String despedida() {
+		return _despedida;
+	}
+	public String vocativo() {
+		return _vocativo;
+	}
+	public String pronome() {
+		return _PronomeTratamento;
+	}
+}
