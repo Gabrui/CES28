00684224091ba41a64e1f28e31128d834d77953c
DylanNS <dylan-ns@hotmail.com>
2017-10-08 14:08:28 -0300

    ModeloArgumentativa

---
+++ Labs/Lab3/src/ex3/ModeloArgumentativa.java
@@ -0,0 +1,20 @@
+/**
+ * LAB-3 / CES-28
+ * @author Dylan N. Sugimoto e Gabriel Adriano de Melo
+ * Data da criação: 08/10/2017
+ */
+package ex3;
+
+/**
+ * @author Dylan N. Sugimoto e Gabriel Adriano de Melo
+ * Classe abstrata que representa o modelo da carta argumentativa.
+ */
+public abstract class ModeloArgumentativa extends Modelo{
+
+	@Override
+	public String despedida(Idioma idioma) {
+		return idioma.despedida() +"\n\n";
+	}
+
+	
+}
