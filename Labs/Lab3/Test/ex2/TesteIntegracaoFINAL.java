49afc455e0b619ec8ccca985c8a71b668d0325a8
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2017-10-09 17:31:35 -0300

    Teste completo Ex2

--- Labs/Lab3/Test/ex2/TesteIntegracaoFINAL.java
+++ Labs/Lab3/Test/ex2/TesteIntegracaoFINAL.java
@@ -34,11 +34,6 @@ public class TesteIntegracaoFINAL {
 				.emprego("Desenvolvedor")
 				.email("gaadrime.melo@gmail.com").fone(fone).idade(20).nomeEmpresa("ITA").build();
 		destinatario = new Pessoa.PessoaBuilder("Dylan", enderecoDestinatario).build();
-		
-		comercial = new CartaComercial(remetente, destinatario, hoje, portugues);
-		pessoal = new CartaPessoal(remetente, destinatario, hoje, portugues);
-		argumentativa = new CartaArgumentativa(remetente, destinatario, hoje, portugues);
-		anonima = new CartaAnonima(destinatario, hoje, portugues);
 	}
 
 	@Test
@@ -47,6 +42,11 @@ public class TesteIntegracaoFINAL {
 		assertNotNull(enderecoRemetente);
 		assertNotNull(destinatario);
 		assertNotNull(remetente);
+
+		anonima = new CartaAnonima(destinatario, hoje, portugues);
+		argumentativa = new CartaArgumentativa(remetente, destinatario, hoje, portugues);
+		comercial = new CartaComercial(remetente, destinatario, hoje, portugues);
+		pessoal = new CartaPessoal(remetente, destinatario, hoje, portugues);
 		
 		assertNotNull(comercial);
 		assertNotNull(pessoal);
@@ -56,6 +56,7 @@ public class TesteIntegracaoFINAL {
 	
 	@Test
 	public void testaModeloAnonimoPortugues() {
+		anonima = new CartaAnonima(destinatario, hoje, portugues);
 		assertEquals(
 				"H8B 205, São José, SP, Brasil\n" + 
 				"1 de 10 de 2017\n" + 
@@ -69,6 +70,7 @@ public class TesteIntegracaoFINAL {
 	
 	@Test
 	public void testaModeloArgumentativoPortugues() {
+		argumentativa = new CartaArgumentativa(remetente, destinatario, hoje, portugues);
 		assertEquals(
 				"Gabriel\n" + 
 				"H8B 241, São José dos Campos, SP, Brasil\n" + 
@@ -89,6 +91,7 @@ public class TesteIntegracaoFINAL {
 	
 	@Test
 	public void testaModeloComercialPortugues() {
+		comercial = new CartaComercial(remetente, destinatario, hoje, portugues);
 		assertEquals(
 				"ITA\n" + 
 				"Gabriel\n" + 
@@ -109,13 +112,14 @@ public class TesteIntegracaoFINAL {
 				"            __________________\n" + 
 				"            Gabriel\n" + 
 				"            Desenvolvedor\n" + 
-				"            +55 (0)  (12) 3947-7891\n" + 
+				"            +(55) (012) 3947-7891\n" + 
 				"            email:gaadrime.melo@gmail.com",
 				comercial.modelo());
 	}
 	
 	@Test
-	public void testaModeloPessoal() {
+	public void testaModeloPessoalPortugues() {
+		pessoal = new CartaPessoal(remetente, destinatario, hoje, portugues);
 		assertEquals(
 				"H8B 205, São José, SP, Brasil\n" + 
 				"1 de 10 de 2017\n" + 
@@ -124,6 +128,86 @@ public class TesteIntegracaoFINAL {
 				"\n" + 
 				"\n" + 
 				"Atensiosamente,\n" + 
+				"Gabriel",
+				pessoal.modelo());
+	}
+	
+
+	
+	@Test
+	public void testaModeloAnonimoIngles() {
+		anonima = new CartaAnonima(destinatario, hoje, ingles);
+		assertEquals(
+				"H8B 205, São José   SP Brasil\n" + 
+				"10/1/2017\n" + 
+				"\n" + 
+				"Mr. Dylan,\n" + 
+				"\n" + 
+				"\n" + 
+				"Gratefully.",
+				anonima.modelo());
+	}
+	
+	@Test
+	public void testaModeloArgumentativoIngles() {
+		argumentativa = new CartaArgumentativa(remetente, destinatario, hoje, ingles);
+		assertEquals(
+				"Gabriel\n" + 
+				"H8B 241, São José dos Campos   SP Brasil\n" + 
+				"\n" + 
+				"10/1/2017\n" + 
+				"\n" + 
+				"Dylan\n" + 
+				"H8B 205, São José   SP Brasil\n" + 
+				"\n" + 
+				"\n" + 
+				"Mr. Dylan,\n" + 
+				"\n" + 
+				"\n" + 
+				"Gratefully,\n" + 
+				"Gabriel",
+				argumentativa.modelo());
+	}
+	
+	@Test
+	public void testaModeloComercialIngles() {
+		comercial = new CartaComercial(remetente, destinatario, hoje, ingles);
+		assertEquals(
+				"ITA\n" + 
+				"Gabriel\n" + 
+				"H8B 241, São José dos Campos   SP Brasil\n" + 
+				"\n" + 
+				"10/1/2017\n" + 
+				"\n" + 
+				"Dylan\n" + 
+				"H8B 205, São José   SP Brasil\n" + 
+				"\n" + 
+				"\n" + 
+				"Mr. Dylan,\n" + 
+				"\n" + 
+				"\n" + 
+				"Gratefully,\n" + 
+				"Gabriel\n" + 
+				"\n" + 
+				"            __________________\n" + 
+				"            Gabriel\n" + 
+				"            Desenvolvedor\n" + 
+				"            +55 (0) (12) 394-77891\n" + 
+				"            email:gaadrime.melo@gmail.com",
+				comercial.modelo());
+	}
+	
+	@Test
+	public void testaModeloPessoalIngles() {
+		pessoal = new CartaPessoal(remetente, destinatario, hoje, ingles);
+		assertEquals(
+				"H8B 205, São José   SP Brasil\n" + 
+				"10/1/2017\n" + 
+				"\n" + 
+				"Mr. Dylan,\n" + 
+				"\n" + 
+				"\n" + 
+				"Gratefully,\n" + 
 				"Gabriel",
 				pessoal.modelo());
 	}
