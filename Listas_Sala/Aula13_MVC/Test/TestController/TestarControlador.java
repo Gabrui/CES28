8989efeb389d99bd3174e9e1b61fbf11d88ec3a7
DylanNS <dylan-ns@hotmail.com>
2017-10-30 16:50:05 -0200

    test

---
+++ Listas_Sala/Aula13_MVC/Test/TestController/TestarControlador.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package TestController;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Dylan N. Sugimoto e Gabriel Adriano de Melo
+ * Classe que testa o controlador
+ */
+public class TestarControlador {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
