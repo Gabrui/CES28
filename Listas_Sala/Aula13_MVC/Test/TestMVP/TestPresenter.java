0eb8b5fcde07fa27caa4c990993602c5328fe807
DylanNS <dylan-ns@hotmail.com>
2017-10-31 09:42:34 -0200

    pkg test mvp

---
+++ Listas_Sala/Aula13_MVC/Test/TestMVP/TestPresenter.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package TestMVP;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Dylan N. Sugimoto
+ *
+ */
+public class TestPresenter {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
