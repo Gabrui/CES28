cc644362b50e17dc7614c9ba2fd93908207f494e
DylanNS <dylan-ns@hotmail.com>
2017-11-03 16:05:48 -0200

    Requisito 9
    Imposto é interface. DP Strategy.

--- Labs/Lab4/src/bancoDados/Imposto.java
+++ Labs/Lab4/src/bancoDados/Imposto.java
@@ -7,7 +7,10 @@ package bancoDados;
  * @author Dylan N. Sugimoto, Gabriel Adriano de Melo e André Marcello Soto Riva Figueira.
  *
  */
-public abstract class Imposto {
+public interface Imposto {
 
-	abstract public int taxar(ProdutoServico analisado);
+	//Imposto é um elemento abstrato para podermos usar o polimorfismo
+	//no BD. Assim podem existir diferentes implementacoes do Imposto que sao usados
+	//pelo BD e definidos a tempo de execucao. DP Strategy. (Requisito 9)
+	public int taxar(ProdutoServico analisado);
 }
