d8f7ac81559df66c51b0fff20e468ed9677c4f6a
DylanNS <dylan-ns@hotmail.com>
2017-11-03 15:20:18 -0200

    Requisito 7
    Id unico fornecido pelo BD, apenas NotaFiscalImutavel possui, pois
    apenas NotaFiscal validada possui.

--- Labs/Lab4/src/notaFiscal/NotaFiscalImutavel.java
+++ Labs/Lab4/src/notaFiscal/NotaFiscalImutavel.java
@@ -5,11 +5,11 @@ import bancoDados.VerificadorCPF;
 
 public class NotaFiscalImutavel extends NotaFiscal{
 
-	private int _ID;
+	private int _ID; //Possui um id unico que é um inteiro sequencial dado pelo BD (Requisito 7).
 	public NotaFiscalImutavel(BD bancoRemoto, VerificadorCPF verificador, String CPF, String item, int quant,int id) {
 		super(bancoRemoto, verificador, CPF, item, quant);
 		// TODO Auto-generated constructor stub
-		_ID = id;
+		_ID = id;//id unico que é um inteiro sequencial dado pelo BD (Requisito 7).
 	}
 
 	
