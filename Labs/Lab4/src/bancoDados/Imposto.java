9d83497ea6ba268946a6ece1b2e4fa33aa8c4c65
DylanNS <dylan-ns@hotmail.com>
2017-11-03 14:06:02 -0200

    classe Imposto

---
+++ Labs/Lab4/src/bancoDados/Imposto.java
@@ -0,0 +1,13 @@
+/**
+ *  @author Dylan N. Sugimoto, Gabriel Adriano de Melo e André Marcello Soto Riva Figueira.
+ */
+package bancoDados;
+
+/**
+ * @author Dylan N. Sugimoto, Gabriel Adriano de Melo e André Marcello Soto Riva Figueira.
+ *
+ */
+public abstract class Imposto {
+
+	abstract public int taxar(ProdutoServico analisado);
+}
