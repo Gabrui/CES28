d75cebb13bc430f873a9cc0343036c9d61808d13
DylanNS <dylan-ns@hotmail.com>
2017-11-03 15:35:58 -0200

    Requisito 8.
    BD sabe calcular imposto.
    BD sabe todos os impostos pela categoria.
    BD passa os impostos cobrados na NotaFiscalImutavel.
    NotaFiscalImutavel sabe os impostos cobrados e seus valores e sabe
    imprimir eles na tela.

--- Labs/Lab4/src/bancoDados/BD.java
+++ Labs/Lab4/src/bancoDados/BD.java
@@ -10,11 +10,14 @@ public interface BD {
 	public Cliente getCliente(String CPF);
 	
     String categoriaPS(ProdutoServico ps);//BD sabe a categoria de cada P/S (Requisito 5)
-    int calculaImposto(ProdutoServico ps);//BD calcula o imposto de cada P/S
+    
     void storeEstado(ProdutoServico ps);//BD guarda o estado de cada P/S para taxar.
-    Imposto getRegra(String categoria);//BD sabe todas as regras de imposto.
+    
     DataObject getDadosPS(ProdutoServico ps);//BD guarda os dados de todos P/S. (Requisito 5)
     
     NotaFiscalImutavel validaNotaFiscal(NotaFiscalMutavel nota);//BD valida NotaFiscal
     //e submete na prefeitura, e se validada retorna NotaFiscalImutavel (Requisito 6)
+    
+    int calculaImposto(ProdutoServico ps);//BD calcula o imposto de cada P/S (Requisito 8)
+    Imposto getRegra(String categoria);//BD sabe todas as regras de imposto. (Requisito 8)
 }
--- Labs/Lab4/src/notaFiscal/NotaFiscalImutavel.java
+++ Labs/Lab4/src/notaFiscal/NotaFiscalImutavel.java
@@ -1,16 +1,31 @@
 package notaFiscal;
 
+import java.util.HashMap;
+
 import bancoDados.BD;
 import bancoDados.VerificadorCPF;
 
 public class NotaFiscalImutavel extends NotaFiscal{
 
 	private int _ID; //Possui um id unico que é um inteiro sequencial dado pelo BD (Requisito 7).
-	public NotaFiscalImutavel(BD bancoRemoto, VerificadorCPF verificador, String CPF, String item, int quant,int id) {
+	private HashMap<String, Integer> _taxasCobradas;//Requisito 8. NotaFiscal sabe dos impostos cobrados.
+	
+	public NotaFiscalImutavel(BD bancoRemoto, VerificadorCPF verificador, String CPF, String item, int quant,int id,HashMap<String, Integer> ImpostosCobrados) {
 		super(bancoRemoto, verificador, CPF, item, quant);
 		// TODO Auto-generated constructor stub
 		_ID = id;//id unico que é um inteiro sequencial dado pelo BD (Requisito 7).
+		_taxasCobradas = ImpostosCobrados;//BD passa para NotaFiscal quais impostos e seus valores foram cobrados. (Requisito 8)
 	}
-
-	
+	//Requisito 8. NotaFiscal sabe dos impostos cobrados e sabe o valor deles.
+	public int valorCobradoImposto(String nomeImposto) {
+		
+		return _taxasCobradas.get(nomeImposto);
+	}
+	//Requisito 8. NotaFiscal sabe dos impostos cobrados e sabe imprimir eles.
+	public void imprimirImpostoCobrado() {
+		
+		System.out.println("Impostos cobrados: ");
+		for (String key : _taxasCobradas.keySet())
+			System.out.print("\n     "+key+_taxasCobradas.get(key).toString());
+	}
 }
