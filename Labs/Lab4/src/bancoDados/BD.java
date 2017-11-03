10526c75e8f5d54ddcc75a7513b0281a70b2c9c2
DylanNS <dylan-ns@hotmail.com>
2017-11-03 13:55:30 -0200

    BD sabe todos impostos e os dataObjects

--- Labs/Lab4/src/bancoDados/BD.java
+++ Labs/Lab4/src/bancoDados/BD.java
@@ -1,6 +1,10 @@
 package bancoDados;
 
+import java.util.List;
+
 public interface BD {
+	private List<DataObject> _tabelaPS;
+	private List<Imposto> _tabelaImposto;
 	public ProdutoServico getProdutoServico(String nome);
 	public Cliente getCliente(String CPF);
 }
