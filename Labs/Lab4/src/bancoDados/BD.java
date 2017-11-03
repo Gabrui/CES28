750f799c875c76cb7b586d894999c9b71b25ceb4
DylanNS <dylan-ns@hotmail.com>
2017-11-03 14:28:28 -0200

    BD sabe a categoria de cada P/S

--- Labs/Lab4/src/bancoDados/BD.java
+++ Labs/Lab4/src/bancoDados/BD.java
@@ -3,8 +3,11 @@ package bancoDados;
 import java.util.List;
 
 public interface BD {
-	private List<DataObject> _tabelaPS;
-	private List<Imposto> _tabelaImposto;
 	public ProdutoServico getProdutoServico(String nome);
 	public Cliente getCliente(String CPF);
+    String categoriaPS(ProdutoServico ps);//BD sabe a categoria de cada P/S
+    int calculaImposto(ProdutoServico ps);//BD calcula o imposto de cada P/S
+    void storeEstado(ProdutoServico ps);//BD guarda o estado de cada P/S para taxar.
+    Imposto getRegra(String categoria);//BD sabe todas as regras de imposto.
+    DataObject getDadosPS(ProdutoServico ps);//BD guarda os dados de todos P/S.
 }
